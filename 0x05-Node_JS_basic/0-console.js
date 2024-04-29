c0c6719cb21537f34f97e38d6b784daf9b256b8e
DarkShadowG17 <rouissiyazh@gmail.com>
2024-04-29 20:06:43 +0100

    nodejs basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Mohamed Zouhair Rouissiya <https://github.com/DarkShadowG17>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
